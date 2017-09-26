43aa25a90a46297b56da5a366c448dcab8d7f0e8
Nadege <nrouelle@outlook.fr>
2017-09-26 15:04:57 +0200

    [Calcul] Traite le cas ou la chaine d'entrée est vide ou nulle

---
+++ StringCalculator/Calculator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace StringCalculator
+{
+    internal class Calculator
+    {
+        public Calculator()
+        {
+        }
+
+        internal double? Add(string numbers)
+        {
+            if(string.IsNullOrEmpty(numbers))
+            {
+                return 0;
+            }
+            return null;
+        }
+    }
+}
--- StringCalculator/CalculatorTest.cs
+++ StringCalculator/CalculatorTest.cs
@@ -5,6 +5,11 @@ namespace StringCalculator
     [TestFixture]
     public class CalculatorTest
     {
-        
+        [Test]
+        public void EmptyStringShouldReturnZeroWhenAdd()
+        {
+            var calculator = new Calculator();
+            Assert.AreEqual(calculator.Add(""), 0);
+        }
     }
 }
